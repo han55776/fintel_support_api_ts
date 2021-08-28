42f368814217412a350fed16deeb6f9085d213e0
han55776 <61185202+han55776@users.noreply.github.com>
2021-08-27 17:55:05 -0700

    Update zendeskClient.ts

--- test/zendeskClient.ts
+++ test/zendeskClient.ts
@@ -1,9 +1,5 @@
 import Client from "../src/Client";
-// const zdClient = new Client({
-//   username: "ysong@nautilusdigital.com",
-//   password: "Han2481211!",
-//   remoteUri: "https://nautilusdigitalgroup.zendesk.com/api/v2/",
-// }).createClient();
+
 
 const zdClient = Client.createClient({
   username: "",
